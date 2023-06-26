d346bc112c438f634f1727d01f85c70d4a49024a
Iryna Rozmaita <iriszechka@ukr.net>
2023-06-26 13:52:11 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    <div>
+      <nav>
+        <NavLink to="/">Home page</NavLink>
+        <NavLink to="/movies">Movies</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home page</div>}/>
+        <Route path="/movies" element={<div>Movies</div>} />
+        <Route path="/movies/:movieId" element={<div>Kollection element</div>}/>
+    </Routes>
+  </div>
+  )  
 };
