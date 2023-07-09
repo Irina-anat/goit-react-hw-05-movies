a72923b908d4e347ce059ccd91e292d8d8e9fec5
Irina-anat <116644910+Irina-anat@users.noreply.github.com>
2023-07-09 12:13:06 +0000

    Deploying to gh-pages from @ Irina-anat/goit-react-hw-05-movies@1800449fa34915f4eb91bd6c0b12450f9e5e048d 🚀

--- static/js/186.2e90c8cb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[186],{186:function(e,i,t){t.r(i);var r=t(689),s=t(184);i.default=function(){var e=(0,r.UO)().movieId;return(0,s.jsxs)("div",{children:["\u0456\u043d\u0444\u043e\u0440\u043c\u0430\u0446\u0456\u044f \u043f\u0440\u043e \u043e\u0433\u043b\u044f\u0434\u0438: ",e]})}}}]);
-//# sourceMappingURL=186.2e90c8cb.chunk.js.map
